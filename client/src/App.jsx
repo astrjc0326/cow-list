f6170608df2007f3cbb8015c221a7bbff8fdc651
王信安 <annywang@wangxinandeMBP.attlocal.net>
2022-03-26 14:25:54 -0700

    create the App component

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div>Hello World!!!</div>
+    )
+  }
+}
+
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.jsx'
 
-ReactDOM.render(<div>Hello World!</div>, document.getElementById('app'));
+ReactDOM.render(<div>Hello World<App /></div>, document.getElementById('app'));
