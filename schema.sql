e021c631a2dab0fbfb8b1f6dd8fdfb3e5fb451e6
Anny Wang <astrjc.anny@gmail.com>
2022-03-26 15:52:39 -0700

    finished making cowlist schema

--- schema.sql
+++ schema.sql
@@ -4,3 +4,18 @@
 -- CREATE YOUR DATABASE
 -- CREATE YOUR TABLES
 -- ADD RECORDS TO YOUR TABLE
+
+DROP DATABASE IF EXISTS cowlist;
+
+CREATE DATABASE cowlist;
+
+USE cowlist;
+
+CREATE TABLE cows (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  description varchar(255),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO cows (name, description) VALUES ('Calvin', 'is A cow');
